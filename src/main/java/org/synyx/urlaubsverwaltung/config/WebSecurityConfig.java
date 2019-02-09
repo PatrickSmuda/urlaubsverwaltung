de98e67dfebbdc0de374e97cac88ba0fedc6d14f
Christian Lange <lange@synyx.de>
2019-02-09 13:12:24 +0100

    translated spring-security.xml into java-configuration
    #455

--- src/main/java/org/synyx/urlaubsverwaltung/config/WebSecurityConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/config/WebSecurityConfig.java
@@ -22,12 +22,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         super.configure(http);
 
         http.authorizeRequests()
-            .mvcMatchers("/**").authenticated()
+            //.mvcMatchers("/**").authenticated()
+            // API
+            .mvcMatchers("/api/sicknotes/**").hasAuthority("OFFICE")
+            .mvcMatchers("/api/**").hasAuthority("USER")
+            // WEB
+            .mvcMatchers("/web/overview").hasAuthority("USER")
+            .mvcMatchers("/web/application/**").hasAuthority("USER")
+            .mvcMatchers("/web/sicknote/**").hasAuthority("USER")
+            .mvcMatchers("/web/staff/**").hasAuthority("USER")
+            .mvcMatchers("/web/overtime/**").hasAuthority("USER")
+            .mvcMatchers("/web/department/**").hasAuthority("USER")
+            .mvcMatchers("/web/settings/**").hasAuthority("USER")
+            .mvcMatchers("/web/google-api-handshake/**").hasAuthority("USER")
+            // sprint boot actuator
+            .mvcMatchers("${management.context-path}/health").permitAll()
+            .mvcMatchers("${management.context-path}/**").hasAnyAuthority("${management.security.roles}")
+            // OPEN
             .and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/web/overview", false).failureForwardUrl("/login?login_error=1")
             .and()
             .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
             .and()
             .csrf().disable()
+            // TODO: dev-env
             .headers().disable();
     }
 
